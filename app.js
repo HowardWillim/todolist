0f2effce0bd731a1da21aeb048012a725b59b94a
HowardWillim <kurolimzz@gmail.com>
2023-03-07 20:53:56 +0700

    adding header and footer

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
-var items=[];
+let items=[];
+let works=[]
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
+
 app.get("/",(req,res)=>{
     var today=new Date();
     var options={
@@ -19,10 +21,21 @@ app.get("/",(req,res)=>{
     res.render("list",{kindOfDay:day,values: items})
 })
 
+app.get("/Work",(req,res)=>{
+    res.render("list",{kindOfDay:"Work",values:works})
+})
+
 app.post("/",(req,res)=>{
     var item =req.body.newItem;
+    if(req.body.list = 'Work'){
+        works.push(item);
+        res.redirect("/Work")
+    }
+    else{
+    
+
     items.push(item);
-    res.redirect("/")
+    res.redirect("/")}
 })
 app.listen(3000,()=>{
     console.log("This is running on port 3000");
